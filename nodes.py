bcfd4cb7ede8799373716fe8bafd8eacf578dfc6
jhj0517 <97279763+jhj0517@users.noreply.github.com>
2025-02-09 20:48:54 +0900

    Fix audio output accessing bug & add GetAudioFilePath

--- nodes.py
+++ nodes.py
@@ -2,21 +2,16 @@ import os
 from typing import Union, BinaryIO, Dict, List, Tuple, Optional
 import time
 import torch
+import sphn
 
 import folder_paths
 from comfy.utils import ProgressBar
 
 from .modules.hibiki_inferencer import HibikiInferencer
 
-# To fix https://github.com/jhj0517/ComfyUI-hibiki/issues/1
-import torch._dynamo
-torch._dynamo.config.suppress_errors = True
-
-
-
 custom_nodes_script_dir = os.path.dirname(os.path.abspath(__file__))
+# custom_nodes_output_dir = os.path.join(folder_paths.get_output_directory(), "my-custom-nodes")
 # custom_nodes_model_dir = os.path.join(folder_paths.models_dir, "my-custom-nodes")
-custom_nodes_output_dir = os.path.join(folder_paths.get_output_directory(), "my-custom-nodes")
 
 
 def get_category_name():
@@ -24,13 +19,11 @@ def get_category_name():
 
 
 class HibikiModelLoader:
-    #  Define the input parameters of the node here.
     @classmethod
     def INPUT_TYPES(s):
         repo_ids = ["kyutai/hibiki-1b-pytorch-bf16", "kyutai/hibiki-2b-pytorch-bf16"]
 
         return {
-            #  If the key is "required", the value must be filled.
             "required": {
                 "repo_id": (repo_ids,),
                 "device": (['cuda', 'cpu'],),
@@ -77,17 +70,84 @@ class SpeechToSpeechTranslation:
 
     def predict(self,
                 model: HibikiInferencer,
-                audio: Union[dict, torch.Tensor],
+                audio: Union[dict, str],
                 batch_size: int,
                 cfg_coef: float,
                 ):
         if isinstance(audio, dict):
             audio, sr = audio["waveform"], audio["sample_rate"]
-        result, sr = model.predict(
+        else:
+            audio, sr = audio, None
+
+        outputs, sr = model.predict(
             input=audio,
+            sample_rate=sr,
             batch_size=batch_size,
             cfg_coef=cfg_coef,
         )
 
-        return ({"waveform": result, "sample_rate": sr},)
+        # Temporary pick first item only in ComfyUI.
+        # The issue is about https://github.com/jhj0517/ComfyUI-hibiki/issues/3
+        first_output_audio = outputs[0]
+        first_output_audio = first_output_audio.unsqueeze(0).float()
 
+        return ({"waveform": first_output_audio, "sample_rate": sr},)
+
+
+class GetAudioFilePath:
+    @classmethod
+    def INPUT_TYPES(cls):
+        return {
+            "required": {
+                "audio_file": ("STRING", {"default": None}),  # Adds an audio upload widget
+            }
+        }
+
+    RETURN_TYPES = ("STRING", )
+    RETURN_NAMES = ("audio",)
+    FUNCTION = "return_str"
+    CATEGORY = get_category_name()
+
+    def return_str(self, audio_file: str):
+        return (audio_file, )
+
+
+# This node is ported from https://github.com/Kosinkadink/ComfyUI-VideoHelperSuite/blob/8629188458dc6cb832f871ece3bd273507e8a766/videohelpersuite/nodes.py#L622
+# Which is Kosinkadink's ComfyUI-VideoHelperSuite node.
+class LoadAudioSphn:
+    @classmethod
+    def INPUT_TYPES(s):
+        input_dir = folder_paths.get_input_directory()
+        files = []
+        audio_extensions = ['mp3', 'mp4', 'wav', 'ogg']
+        for f in os.listdir(input_dir):
+            if os.path.isfile(os.path.join(input_dir, f)):
+                file_parts = f.split('.')
+                if len(file_parts) > 1 and (file_parts[-1] in audio_extensions):
+                    files.append(f)
+        return {"required": {
+                "audio": (sorted(files),),
+            },
+        }
+
+    CATEGORY = get_category_name()
+
+    RETURN_TYPES = ("AUDIO",)
+    RETURN_NAMES = ("audio",)
+    FUNCTION = "load_audio_sphn"
+
+    def load_audio_sphn(self, **kwargs):
+        audio_file = folder_paths.get_annotated_filepath(self.strip_path(kwargs['audio']))
+        return (self.read_with_sphn(audio_file, None), )
+
+    def read_with_sphn(self, audio: str, sample_rate):
+        return sphn.read(audio, sample_rate=sample_rate)
+
+    @staticmethod
+    def strip_path(path):
+        path = path.strip()
+        if path.startswith("\""):
+            path = path[1:]
+        if path.endswith("\""):
+            path = path[:-1]
+        return path
